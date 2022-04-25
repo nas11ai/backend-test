e092ce96b3038f3252a24c3e486bb88451441284
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-25 12:11:52 +0800

    adding mongoose and applying its functionality

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
+// if (process.argv.length < 3) {
+//     console.log('Please provide the password as an argument: node mongo.js <password>')
+// }
+
+// const password = process.argv[2]
+
+// const url = `mongodb+srv://nas11ai:cvIfvmHIc9pZuxV9@cluster0.jap1z.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//     content: String,
+//     date: Date,
+//     important: Boolean
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
+
+// noteSchema.set('toJSON', {
+//     transform: (document, returnedObject) => {
+//         returnedObject.id = returnedObject._id.toString();
+//         delete returnedObject._id
+//         delete returnedObject.__v
+//     }
+// })
+
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-01-10T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-01-10T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-01-10T19:20:14.298Z",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         date: "2022-01-10T17:30:31.098Z",
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: "Browser can execute only Javascript",
+//         date: "2022-01-10T18:39:34.091Z",
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         date: "2022-01-10T19:20:14.298Z",
+//         important: true
+//     }
+// ]
 
 app.use(express.json())
 app.use(cors())
@@ -45,41 +73,37 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
         id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save()
+        .then(savedNote => response.json(savedNote))
 })
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+// app.delete('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
-})
+//     response.status(204).end()
+// })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).send('404 Not Found').end()
-    }
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
