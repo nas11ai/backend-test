45b911eea85032be627bb5c5dd226b6915a29e43
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-26 16:28:10 +0800

    adding validation

--- models/note.js
+++ models/note.js
@@ -10,8 +10,15 @@ mongoose.connect(url)
     .catch(error => console.log(`error connecting to MongoDB: ${error.message}`))
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean
 })
 
